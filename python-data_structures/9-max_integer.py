db4734f7810ab1af0ea5f4826d1044da75909d9b
bricorne <bricepoitevineau@gmail.com>
2023-06-06 05:13:55 -0700

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxn = 0
     if len(my_list) == 0:
         return None
+    maxn = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > maxn:
             maxn = my_list[i]
