5971d6248553a0eca300c8da84d2b0b6f5107838
bricorne <bricepoitevineau@gmail.com>
2023-06-12 08:03:15 -0700

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Square"""
-    def __init__(self,size=0):
+    def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
