07f0b83b158e0b06c3706bcc079749984bb55458
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-06-20 16:20:27 +0200

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class"""
+    def area(self):
+        """define area"""
+        raise Exception("area() is not implemented")
