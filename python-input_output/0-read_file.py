aa8ad91a03d5d5a5754ef3a2ac4d0e1ccad7682d
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-06-23 13:05:53 +0200

    task 1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""read file"""
+
+
 def read_file(filename=""):
+    """read file"""
     with open(filename, "r", encoding="utf8") as fichier:
         for ligne in fichier:
             print(ligne, end="")
