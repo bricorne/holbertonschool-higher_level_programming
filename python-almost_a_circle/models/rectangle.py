f3977bbb84d28e7901625f1dea2a0902de23ca66
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-06-28 16:09:12 +0200

    task 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,6 @@ class Rectangle(Base):
         """print the rectangle"""
         for i in range(self.__height):
             print('#' * self.__width)
+    
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
