73881025a284a9586a956416fb8f648f96dd5074
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-06-28 12:07:23 +0200

    main

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Class Base"""
+"""create class Base"""
 
 
 class Base:
-    """base"""
+    """class base"""
     __nb_objects = 0
     def __init__(self, id=None):
         if id is not None:
