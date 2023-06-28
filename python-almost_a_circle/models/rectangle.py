197fddcb34eee77ad33b437a5abfa0821fb5f072
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-06-28 15:35:09 +0200

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,31 @@ class Rectangle(Base):
     """class rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """ width """
+        return self.__width
+
+    @property
+    def height(self):
+        """height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """ x """
+        return self.__x
+
+    @property
+    def y(self):
+        """ y """
+        return self.__y
+
+    def area(self):
+        """area of rectangle"""
+        return self.__width * self.__height
