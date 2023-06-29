fc8f4e4ca1a569dd7e5bf353deca0e5d5cc8eadb
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-06-29 15:00:38 +0200

    task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -31,3 +31,10 @@ class Base:
         
         with open(filename, 'w') as file:
             file.write(json_string)
+    
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
