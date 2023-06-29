61cba6dd41f57ca3c8ebe7aafb3593cebb2c542e
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-06-29 15:52:20 +0200

    task 18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,8 +17,7 @@ class Base:
         """returns the JSON string"""
         if list_dictionaries is None:
             return "[]"
-        else :
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
         
     @classmethod
     def save_to_file(cls, list_objs):
@@ -38,3 +37,14 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+
+        dummy.update(**dictionary)
+        return dummy
