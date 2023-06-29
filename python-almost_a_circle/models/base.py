4874720516c0d8656e01e81de66a35f890b6d21c
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-06-29 13:27:02 +0200

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,6 +15,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """returns the JSON string"""
-        if list_dictionaries is None or not list_dictionaries:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
