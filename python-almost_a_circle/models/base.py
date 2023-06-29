1121357537b74e79b28e8bf74db79c5c7c0b7986
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-06-29 13:37:20 +0200

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,7 +11,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+    
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string"""
         if list_dictionaries is None:
